b25495336d29fdf069478bb8885a11f3a9f7717b
Frankly <Frankly.codes@gmail.com>
2025-03-14 20:40:05 -0600

    Initial Commit

---
+++ Roulette/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Roulette
+//
+//  Created by Frankly on 14/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Roulette/RouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouletteApp.swift
+//  Roulette
+//
+//  Created by Frankly on 14/03/25.
+//
+
+import SwiftUI
+
+@main
+struct RouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
