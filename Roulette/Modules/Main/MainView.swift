dcba3564d777976f022482c05cd83fd33df6d61b
Frankly <Frankly.codes@gmail.com>
2025-06-24 21:52:29 -0600

    SCRUM-15: * SplashScreen implementation * Image header implementation

--- Roulette/Modules/Main/MainView.swift
+++ Roulette/Modules/Main/MainView.swift
@@ -9,22 +9,62 @@ import SwiftUI
 
 struct MainView: View {
     @ObservedObject var controller = MainController()
+    @State private var showLaunchScreen = true
+    @State private var showAppContent = false
     
     var body: some View {
         ZStack {
             VStack {
-                SettingsButtonView(controller: controller)
+                ZStack {
+                    HStack {
+                        Spacer()
+                        Images.ROULETTE
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: Constants.ScreenSize.screenSize.width * 0.3)
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        Spacer()
+                        SettingsButtonView(controller: controller)
+                    }
+                }
+                .padding(.top, 60)
+                
                 Spacer()
                 TitleModuleView(controller: controller)
                 SpinnerModuleView(controller: controller)
                 Spacer()
                 EditButtonModuleView(controller: controller)
+                    .padding()
             }
             .blur(radius: controller.showEdit ? 9 : 0)
             .ignoresSafeArea()
+            .opacity(showAppContent ? 1 : 0)
+            .scaleEffect(showAppContent ? 1 : 0.5)
+            .animation(.easeOut(duration: 0.6), value: showAppContent)
             
             if controller.showEdit {
                 ItemsView(mainController: controller, itemsController: controller.itemsController)
+                    .padding()
+            }
+            
+            if showLaunchScreen {
+                LaunchScreenView(
+                    onStartExit: {
+                        withAnimation {
+                            showAppContent = true
+                        }
+                    },
+                    onFinish: {
+                        withAnimation {
+                            showLaunchScreen = false
+                        }
+                    }
+                )
+                .transition(.opacity)
+                .zIndex(1)
             }
         }
         .fullScreenCover(isPresented: $controller.showSettings) {
@@ -34,7 +74,7 @@ struct MainView: View {
             SpinningBackgroundView()
                 .scaleEffect(2.0)
                 .ignoresSafeArea()
-                .blur(radius: (controller.showEdit||controller.showResult) ? 9 : 0)
+                .blur(radius: (controller.showEdit || controller.showResult) ? 9 : 0)
         )
     }
 }
